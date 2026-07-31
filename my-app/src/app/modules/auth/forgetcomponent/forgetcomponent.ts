25670fa542acb01b06ac62e379ad606ab14964ff
Dheeraj Mishra <dheerajpandatbdps@gmail.com>
2026-07-31 11:54:29 +0530

    login forget & signup form added

--- my-app/src/app/modules/auth/forgetcomponent/forgetcomponent.ts
+++ my-app/src/app/modules/auth/forgetcomponent/forgetcomponent.ts
@@ -1,9 +1,51 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from 'cats-ui-lib';
 
 @Component({
   selector: 'app-forgetcomponent',
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, InputComponent],
   templateUrl: './forgetcomponent.html',
   styleUrl: './forgetcomponent.scss',
 })
-export class Forgetcomponent {}
+export class Forgetcomponent {
+  otpSent = false;
+  resetSuccess = false;
+
+  emailForm = new FormGroup({
+    email: new FormControl(''),
+  });
+
+  resetForm = new FormGroup({
+    otp: new FormControl(''),
+    newPassword: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+
+  onSendOtp() {
+    console.log('send otp to', this.emailForm.value);
+    this.otpSent = true;
+  }
+
+  onResetPassword() {
+    console.log('reset password', this.resetForm.value);
+    this.resetSuccess = true;
+    this.resetForm.reset();
+  }
+
+  emailConfig: any = {
+    type: 'email',
+    placeholder: 'Enter your registered email',
+  };
+
+  otpConfig: any = {
+    type: 'text',
+    placeholder: 'Enter OTP',
+  };
+
+  passwordConfig: any = {
+    type: 'password',
+    placeholder: 'Enter password',
+  };
+}
--- my-app/src/app/modules/auth/logincomponnet/logincomponnet.ts
+++ my-app/src/app/modules/auth/logincomponnet/logincomponnet.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+import { InputComponent } from 'cats-ui-lib';
 
 @Component({
   selector: 'app-logincomponnet',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, InputComponent],
   templateUrl: './logincomponnet.html',
   styleUrl: './logincomponnet.scss',
 })
-export class Logincomponnet {}
+export class Logincomponnet {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+    rememberMe: new FormControl(false),
+  });
+
+  onLogin() {
+    console.log('login values', this.loginForm.value);
+    this.loginForm.reset();
+  }
+
+  emailConfig: any = {
+    type: 'email',
+    placeholder: 'Enter your email',
+  };
+
+  passwordConfig: any = {
+    type: 'password',
+    placeholder: 'Enter your password',
+  };
+}
