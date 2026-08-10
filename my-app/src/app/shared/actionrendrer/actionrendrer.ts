c9b4b33f8e2b51800e203f9bd516d253609ad901
Dheeraj Mishra <dheerajpandatbdps@gmail.com>
2026-08-10 16:52:58 +0530

    edit profile details form added

--- my-app/src/app/shared/actionrendrer/actionrendrer.ts
+++ my-app/src/app/shared/actionrendrer/actionrendrer.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { DialogBoxService, InputComponent, CustomDatePickerComponent, DateRange, DateTimeResult } from 'cats-ui-lib';
 
 @Component({
   selector: 'app-actionrendrer',
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, InputComponent, CustomDatePickerComponent],
   templateUrl: './actionrendrer.html',
   styleUrl: './actionrendrer.scss',
 })
-export class Actionrendrer {}
+export class Actionrendrer {
+  dialogRef: any;
+  isSubmitted = false;
+
+  @ViewChild('myTemplateupload') template!: TemplateRef<any>;
+
+  constructor(private dialogBoxService: DialogBoxService) {}
+
+  editForm = new FormGroup({
+    fname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    username: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    number: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$')]),
+    pass: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    cpass: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    date: new FormControl(''),
+  });
+
+  inputConfig: any = { type: 'text', placeholder: 'Enter value' };
+  passwordConfig: any = { type: 'password', placeholder: 'Enter password' };
+
+  openModal() {
+    this.dialogRef = this.dialogBoxService.open(this.template, {
+      class: 'edit_modal',
+      closeOnBackdropClick: false,
+    });
+  }
+
+  close() {
+    this.dialogRef?.close();
+    this.editForm.reset();
+    this.isSubmitted = false;
+  }
+
+  displayData($event: string | Date | DateRange | DateTimeResult) {}
+
+  onUpdate() {
+    this.isSubmitted = true;
+    this.editForm.markAllAsTouched();
+    if (this.editForm.invalid) return;
+    console.log('Updated values:', this.editForm.value);
+    this.close();
+  }
+}
